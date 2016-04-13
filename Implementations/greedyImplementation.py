a923c4fb03d4737044eccc26a454e443902fed8f
Jeremy 'LeJyBy' Barbay <jeremy@barbay.cl>
2016-04-13 11:45:16 -0300

    Specified the minimum set of functions required from a Deferred Data Structure on Multiset

--- Implementations/greedyImplementation.py
+++ Implementations/greedyImplementation.py
@@ -1,23 +1,17 @@
 import unittest, doctest, copy, bisect
 
 class PartiallySortedArray(): 
-    """A class receiving an unsorted array, supporting rank, select and partialSum operators on it in the second most greedy way (the partial sums are computed only once). 
+    """A class receiving an unsorted array, supporting rank and select operators on it in the second most greedy way. 
 
     """
 
     def __init__(self,A):
-        """Create a Partially Sorted Array supporting rank, select and partialSum operators from a simple array.
+        """Create a Partially Sorted Array supporting rank and select operators from a simple array.
 
         """
         self.values = sorted(A)
-        self.partialSums = [0]*(len(self.values))
-        partialSum = 0 
-        for i in range(len(self.values)):
-            partialSum +=  self.values[i]      
-            self.partialSums[i] = partialSum
         self.nbRankQueriesProcessed = 0 
         self.nbSelectQueriesProcessed = 0 
-        self.nbPartialSumQueriesProcessed = 0 
             
     def __len__(self):
         """Number of Elements in the Partially Sorted Array.
@@ -68,31 +62,6 @@ class PartiallySortedArray():
         return bisect.bisect_right(self.values, x)
 
     
-    def partialSum(self,r):
-        """Sum of the r smallest elements in the set.
-
->>> S = PartiallySortedArray([50,40,30,20,10])
->>> print(S.partialSum(2))
-30
->>> print(S.partialSum(0))
-0
-"""
-        if r <= 0:
-            return 0
-        else:
-            self.nbPartialSumQueriesProcessed += 1
-            return self.partialSums[r-1]
-
-    def rangeSum(self,left,right):
-        """Sum of the elements which would be in range(left,right) in the sorted array.
-
->>> S = PartiallySortedArray([50,40,30,20,10])
->>> print(S.rangeSum(0,2))
-30
->>> print(S.rangeSum(0,0))
-0
-"""
-        return self.partialSum(right)-self.partialSum(left)
 
     def totalNbOfQueriesPerformed(self):
         """Returns the total number of queries performed on the Partially Sorted Array.
@@ -111,7 +80,7 @@ class PartiallySortedArray():
 2
 
 """
-        return(self.nbRankQueriesProcessed+self.nbSelectQueriesProcessed+self.nbPartialSumQueriesProcessed)
+        return(self.nbRankQueriesProcessed+self.nbSelectQueriesProcessed)
         
 def main():
     unittest.main()
